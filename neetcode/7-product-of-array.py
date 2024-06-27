3d14ae27cdc602e75058ec2c3190e4f1a388abf8
Souradeepta <sdb.svnit@gmail.com>
2024-06-27 00:49:08 -0700

    6-27-2024

--- neetcode/7-product-of-array.py
+++ neetcode/7-product-of-array.py
@@ -1,11 +1,48 @@
 from typing import List
-
+import copy
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pass
+        """
+        - can divide by zero
+        """
+        product = 1
+        for num in nums:
+            product *= num
+        print(f"{product=}")
+        for idx, num in enumerate(nums):
+            if num != 0:
+                nums[idx] = product//num
+                print(f"{nums=}")
+        return nums
 
+        # nums2 = copy.deepcopy(nums)
+        # for idx, num in enumerate(nums):
+        #     for i in range(len(nums)):
+        #         nums[idx] = 1
+        #         if i != idx:
+        #             nums[idx] *= nums2[i]
+        #             print(f"{nums}")
 
+        # return nums
+
+    def productExceptSelf2(self, nums: List[int]) -> List[int]:
+        """
+        T :o(n), S: o(1)
+        """
+        result = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            result[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            result[i] *= postfix
+            postfix *= nums[i]
+
+        return result
 if __name__ == "__main__":
-    assert Solution().productExceptSelf([1,2,4,6]) == [48,24,12,8]
-    assert Solution().productExceptSelf([-1,0,1,2,3]) == [0,-6,0,0,0]
+    assert Solution().productExceptSelf2([1,2,4,6]) == [48,24,12,8]
+    assert Solution().productExceptSelf2([-1,0,1,2,3]) == [0,-6,0,0,0]
---
+++ neetcode/8-valid-sudoku.py
@@ -0,0 +1,61 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    # def isValidSudoku(self, board: List[List[str]]) -> bool:
+    #     columns = len(board)
+    #     rows = len(board[0])
+
+    #     # checking columns
+    #     for i in range(rows):
+    #         checker = [0]* columns
+    #         for j in range(columns):
+    #             if board[i][j] != '.' and checker[j] != 0:
+    #                 checker[j] = board[i][j]
+    #             else:
+    #                 return False
+    #     # checking rows
+    #     for i in range(rows):
+    #         checker = [0]* rows
+    #         for j in range(columns):
+    #             if board[j][i] != '.' and checker[j] != 0:
+    #                 checker[j] = board[i][j]
+    #             else:
+    #                 return False
+    #     # checking in grid
+
+    #     return True
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        columns = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or
+                    board[r][c] in columns[c] or
+                    board[r][c] in squares[(r//3, c//3)]):
+                    return False
+                columns[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+        return True
+
+        
+if __name__ == "__main__":
+    board = [
+        ["1","2",".",".","3",".",".",".","."],
+        ["4",".",".","5",".",".",".",".","."],
+        [".","9","8",".",".",".",".",".","3"],
+        ["5",".",".",".","6",".",".",".","4"],
+        [".",".",".","8",".","3",".",".","5"],
+        ["7",".",".",".","2",".",".",".","6"],
+        [".",".",".",".",".",".","2",".","."],
+        [".",".",".","4","1","9",".",".","8"],
+        [".",".",".",".","8",".",".","7","9"]]
+
+    assert Solution().isValidSudoku(board) is True
